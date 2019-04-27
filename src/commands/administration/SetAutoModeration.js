56ede8201eac9d7d8e9c9537c050e03a1098aadb
Asshley <ashwafaw@gmail.com>
2019-04-27 14:59:49 -0400

    feat: automoderation command

---
+++ src/commands/administration/SetAutoModeration.js
@@ -0,0 +1,29 @@
+const { Command } = require('patron.js');
+const StringUtil = require('../../utility/StringUtil.js');
+const messages = require('../../../data/messages.json');
+
+class SetAutoModeration extends Command {
+  constructor() {
+    super({
+      names: ['setautomoderation', 'automod', 'modabuse'],
+      groupName: 'administration',
+      description: 'Toggles the mod abuse feature.'
+    });
+  }
+
+  async run(msg) {
+    const { autoModeration } = msg.dbGuild;
+
+    await msg._client.db.guildRepo.updateGuild(msg.channel.guild.id, {
+      $set: {
+        autoModeration: !autoModeration
+      }
+    });
+
+    return msg.createReply(
+      StringUtil.format(messages.commands.setAutoModeration.successful, !autoModeration)
+    );
+  }
+}
+
+module.exports = new SetAutoModeration();
