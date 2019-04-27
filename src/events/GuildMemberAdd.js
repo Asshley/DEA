a15f370dbd5707a2f4a73a8a3d87e97a6cf5e9d8
Asshley <ashwafaw@gmail.com>
2019-04-26 21:09:19 -0400

    Export an instance of the MemberService class

--- src/events/GuildMemberAdd.js
+++ src/events/GuildMemberAdd.js
@@ -1,10 +1,10 @@
-const MemberService = require('../services/MemberService.js');
 const Event = require('../structures/Event.js');
 const { CLIENT_EVENTS } = require('../utility/Constants.js');
+const memberService = require('../services/MemberService.js');
 
 class GuildMemberAdd extends Event {
   run(_, member) {
-    return MemberService.join(member);
+    return memberService.join(member);
   }
 }
 GuildMemberAdd.EVENT_NAME = CLIENT_EVENTS.GUILD_MEMBER_ADD;
--- src/services/MemberService.js
+++ src/services/MemberService.js
@@ -1,5 +1,5 @@
 class MemberService {
-  static async join(member) {
+  async join(member) {
     const { client } = member.guild.shard;
     const dbGuild = await member.guild.dbGuild();
     const isMuted = await client.db.muteRepo.anyMute(member.id, member.guild.id);
@@ -7,10 +7,10 @@ class MemberService {
     if (dbGuild.roles.muted && isMuted) {
       const role = member.guild.roles.get(dbGuild.roles.muted);
       const clientMember = member.guild.members.get(client.user.id);
-      const needPerms = !role || role.position >= clientMember.highestRole.position
+      const needPerms = role.position >= clientMember.highestRole.position
         || !clientMember.permission.has('manageRoles');
 
-      if (needPerms) {
+      if (!role || needPerms) {
         return;
       }
 
@@ -19,4 +19,4 @@ class MemberService {
   }
 }
 
-module.exports = MemberService;
+module.exports = new MemberService();
